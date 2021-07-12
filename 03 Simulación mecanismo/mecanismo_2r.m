14cbe0604c12845045ef61804d41181b2f422451
Leonardo Fabio <lfmercadobe@unal.edu.co>
2021-07-12 11:50:02 -0500

    Se agrega la cinematica inversa del mecanismo

--- 03 Simulación mecanismo/mecanismo_2r.m
+++ 03 Simulación mecanismo/mecanismo_2r.m
@@ -14,17 +14,10 @@ syms q1 q2
 
 
 %--------------------------------------------------------------------------
-% Cinematica inversa: 
+% Cinematica Directa: 
 x = l2*cos(q1+q2) + l1*cos(q1);
 y = l2*sin(q1+q2) + l1*sin(q1);
 %--------------------------------------------------------------------------
-
-%%
-mdl_planar2
-p2.teach
-
-%%
-p2.plot([0 pi/2])
 
 %%
 L(1) = Link('revolute','alpha', 0,    'a',0 ,   'd',0,  'offset', 0,   'modified', 'qlim',[-pi pi]);
@@ -34,4 +27,27 @@ mecanismo.tool = [0 0 1 l2;
                   1 0 0 0;
                   0 1 0 0;
                   0 0 0 1];
+mecanismo.teach;
+grid on;
+%%
+[q1,q2] = hallar_sol(1.810,0,1,1,1);
 
+disp("El angulo theta_1 es de:")
+disp(rad2deg(q1))
+disp("El angulo theta_2 es de:")
+disp(rad2deg(q2))
+%%
+function [q1,q2] = hallar_sol(x,y,l1,l2,codo)
+% Función para hallar la posición angular que debe adoptar el mecanismo
+% para alcanzar una posición dada.
+
+if(codo)
+    disp("Entramos en codo Abajo");
+    q2 = acos(((x^2) + (y^2) - (l1^2) - (l2^2))/(2*l1*l2));
+    q1 = atan2(y,x) - atan2((l2*sin(q2)),(l1+l2*cos(q2)));
+else
+    disp("Entramos en codo arriba");
+    q2 = -1*acos(((x^2) + (y^2) - (l1^2) - (l2^2))/(2*l1*l2));
+    q1 = atan2(y,x) - atan2((l2*sin(q2)),(l1+l2*cos(q2)));
+end
+end
